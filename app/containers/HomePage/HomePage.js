b1072bbcbcce4a48acad3804f1a86adafc54b63c
Emily Morehouse <emily@cuttlesoft.com>
2019-07-11 20:21:25 -0600

    [#167005457] 📚 (HomePage) Add documentation and comments

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -1,32 +1,59 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- */
-
+// Libraries
 import React, { useContext, useEffect, useState, memo } from 'react'
 import { Helmet } from 'react-helmet'
 import { FormattedMessage } from 'react-intl'
 import { observer } from 'mobx-react'
 
+// Components
 import { Header } from 'components/Header'
 import ReposList from 'components/ReposList'
+
+// Styles
 import { AtPrefix, CenteredSection, Form, Input, Section } from './styles'
+
+// Messages
 import messages from './HomePage.messages'
 
+// Context / Stores
 import { GitHubStoreContext } from '../../stores/GithubStore'
 
+/*
+ * HomePage
+ *
+ * The HomePage renders:
+ * - The page title and metadata using Helmet
+ * - The page header and description
+ * - The search input and repository listing for a given user
+ *
+ * A user can:
+ * - Show Github repositories by username
+ *   - When a user inputs a username into the input field then hits the
+ *     enter key, the form is submitted and search results are retrieved
+ *   - Repositories are displayed using the ReposList component, which handles
+ *     loading and error messages
+ */
 export const HomePage = observer(() => {
   const gitHubStore = useContext(GitHubStoreContext)
   const [searchTerm, setSearchTerm] = useState('')
 
+  /**
+   * Prevent the default behavior from occuring (for example, a page refresh),
+   * then initiate fetching repositories based on the current `searchTerm`
+   *
+   * @param {object} evt The sythetic event from firing the function via
+   *   `onSubmit`
+   */
   const onSubmitForm = evt => {
     evt.preventDefault()
     gitHubStore.getRepos(searchTerm)
   }
 
+  /**
+   * When the initial username from the store is not null, set the local state
+   * for `searchTerm` to the provided username and initiate fetching
+   * repositories
+   */
   useEffect(() => {
-    // When initial state username is not null, submit the form to load repos
     if (gitHubStore.currentUser && gitHubStore.currentUser.trim().length > 0) {
       setSearchTerm(gitHubStore.currentUser)
       gitHubStore.getRepos(gitHubStore.currentUser)
