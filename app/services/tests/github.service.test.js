8fda771cb80a79b09aa5fce0c779b8ef23e9c4bf
Emily Morehouse <emily@cuttlesoft.com>
2019-07-11 22:32:13 -0600

    [#167005457] ☔️ (GitHub Service) Add test for 204 response code

--- app/services/tests/github.service.test.js
+++ app/services/tests/github.service.test.js
@@ -33,3 +33,16 @@ test('getRepos catches an error', async () => {
     expect(error.response.data).toEqual('Error')
   }
 })
+
+test('getRepos returns an empty object if a 204 occurs', async () => {
+  const url = `/users/${TEST_USER}/repos?type=all&sort=updated`
+  const axiosMock = new MockAdapter(axios)
+
+  axiosMock.onGet(url).reply(204, null)
+
+  try {
+    await getRepos(TEST_USER)
+  } catch (error) {
+    expect(error.response.data).toEqual(null)
+  }
+})
