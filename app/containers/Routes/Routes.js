ebed6af6e89f53b4738fabcdd68ae8190ffd57be
Emily Morehouse <emily@cuttlesoft.com>
2020-03-22 17:59:46 -0600

    [LIVE-14] 🔄 (Routes) s/App/Routes; Remove loadables
    - Rename App to Routes
    - Remove loadables and update imported routes

--- app/containers/Routes/Routes.js
+++ app/containers/Routes/Routes.js
@@ -1,47 +1,56 @@
+/* @flow */
+
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { Switch, Route } from 'react-router-dom'
 import styled from 'styled-components'
 
+// Containers
+import { FeaturePage } from 'containers/FeaturePage'
+import { HomePage } from 'containers/HomePage'
+import { NotFoundPage } from 'containers/NotFoundPage'
+
 // Components
-import HomePage from 'containers/HomePage/HomePage.loadable'
-import FeaturePage from 'containers/FeaturePage/FeaturePage.loadable'
-import NotFoundPage from 'containers/NotFoundPage/NotFoundPage.loadable'
+import { Header } from 'components/Header'
 import { TemplateHeader } from 'components/TemplateHeader'
 import { Footer } from 'components/Footer'
 
 // Styles
 import GlobalStyle from '../../global-styles'
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
+const Container = styled.div`
+  margin: 0 auto 30px auto;
   min-height: 100%;
   padding: 0 16px;
-  flex-direction: column;
 `
 /**
  *
- * App
+ * Routes
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
-export default function App() {
+export default function Routes() {
   return (
-    <AppWrapper>
+    <>
       <Helmet titleTemplate="%s - React.js Boilerplate" defaultTitle="React.js Boilerplate">
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      <TemplateHeader />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
+      <Header />
+
+      <Container>
+        <TemplateHeader />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/features" component={FeaturePage} />
+          <Route path="" component={NotFoundPage} />
+        </Switch>
+
+        <Footer />
+      </Container>
+
       <GlobalStyle />
-    </AppWrapper>
+    </>
   )
 }
