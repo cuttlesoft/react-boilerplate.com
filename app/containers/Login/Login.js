dfb111cb089b344815e2b074e593bef34bf62c59
Emily Morehouse <emily@cuttlesoft.com>
2020-04-22 20:42:31 -0600

    [DASH-14] 🔄 (Login) Add banner messages for confirm and password reset

--- app/containers/Login/Login.js
+++ app/containers/Login/Login.js
@@ -1,6 +1,7 @@
 /* @flow */
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { isEmail } from 'validator'
+import PropTypes from 'prop-types'
 
 // Components
 import { Anchor } from 'components/Anchor'
@@ -9,6 +10,7 @@ import { Button } from 'components/Button'
 import { Form, FormField } from 'components/Form'
 import { Heading } from 'components/Heading'
 import { Message } from 'components/Message'
+import { Link } from 'components/Link'
 import { PasswordInput } from 'components/PasswordInput'
 
 import { TextInput } from 'components/TextInput'
@@ -23,12 +25,26 @@ import { UserStoreContext } from '../../stores/UserStore'
  * Login
  *
  */
-const Login = () => {
+const Login = ({ location }) => {
   const { message: error, showMessage: showError } = useFlashMessage(null)
+  const { message: redirectedMessage, showMessage: showRedirectedMessage } = useFlashMessage(null)
 
   const [loading, setLoading] = useState(false)
 
-  const { setCurrentUser } = useContext(UserStoreContext)
+  const { setCurrentUser, setCurrentTokens } = useContext(UserStoreContext)
+
+  /**
+   * Check if the user has been redirected to this page with a URL parameter
+   *
+   * - `confirmed` indicates that a user has been sent to this page from `ConfirmAccount`
+   * - `reset` indicates that a user has been sent to this page from `ResetPassword`
+   */
+  useEffect(() => {
+    if (location && location.search === '?confirmed=true')
+      showRedirectedMessage('Your email address has been confirmed. You may now log in.')
+    if (location && location.search === '?reset=true')
+      showRedirectedMessage('Your password has been changed. You may now log in.')
+  }, [])
 
   /**
    * Helper to determine if the submit button is disabled
@@ -46,9 +62,14 @@ const Login = () => {
         id="login-form"
         validate="blur"
         onSubmit={({ value }) => {
-          login(value, showError, setLoading, setCurrentUser)
+          login(value, showError, setLoading, setCurrentUser, setCurrentTokens)
         }}
       >
+        {/* Redirected Message */}
+        <Box background={{ color: 'status-ok', opacity: 'weak' }} pad={{ horizontal: 'small' }}>
+          {redirectedMessage && <Message message={redirectedMessage} />}
+        </Box>
+
         <FormField
           label="Email"
           name="email"
@@ -79,14 +100,20 @@ const Login = () => {
 
         {/* Submit */}
         <Box direction="row" margin={{ vertical: 'medium' }}>
-          <Button type="submit" label="Login" primary disabled={_isButtonDisabled()} />
+          <Button type="submit" label="Login" disabled={_isButtonDisabled()} />
         </Box>
+
+        <Link to="/password-reset-request">Forgot Password?</Link>
 
         {/* Status Messages */}
         <Box>{error && <Message message={error} isError />}</Box>
       </Form>
     </>
   )
+}
+
+Login.propTypes = {
+  location: PropTypes.object.isRequired,
 }
 
 export default Login
