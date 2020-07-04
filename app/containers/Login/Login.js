15eeb1f20be3bf3664422ff31ba1685f0f54a5ad
Emily Morehouse <emily@cuttlesoft.com>
2020-07-03 22:41:48 -0600

    [CUTL-90] 🔨 (Login) Refactor to include page header, avoid `Anchor`, unify styles

--- app/containers/Login/Login.js
+++ app/containers/Login/Login.js
@@ -4,15 +4,15 @@ import { isEmail } from 'validator'
 import PropTypes from 'prop-types'
 
 // Components
-import { Anchor } from 'components/Anchor'
 import { Box } from 'components/Box'
 import { Button } from 'components/Button'
 import { Form, FormField } from 'components/Form'
+import { FormContainer } from 'components/FormContainer'
 import { Heading } from 'components/Heading'
 import { Message } from 'components/Message'
 import { Link } from 'components/Link'
+import { LogoHeader } from 'components/LogoHeader'
 import { PasswordInput } from 'components/PasswordInput'
-
 import { TextInput } from 'components/TextInput'
 
 // Utils and messages
@@ -38,76 +38,89 @@ const Login = ({ location }) => {
    *
    * - `confirmed` indicates that a user has been sent to this page from `ConfirmAccount`
    * - `reset` indicates that a user has been sent to this page from `ResetPassword`
+   * - `created` indicates that a user has been sent to this page from an email confirmation link
    */
   useEffect(() => {
     if (location && location.search === '?confirmed=true')
       showRedirectedMessage('Your email address has been confirmed. You may now log in.')
-    if (location && location.search === '?reset=true')
+    else if (location && location.search === '?reset=true')
       showRedirectedMessage('Your password has been changed. You may now log in.')
+    else if (location && location.search === '?created=true')
+      showRedirectedMessage('Your account has been created. You may now log in.')
   }, [])
-
-  /**
-   * Helper to determine if the submit button is disabled
-   */
-  const _isButtonDisabled = () => loading
 
   return (
     <>
-      <Box align="center" pad="large" data-testid="login-header">
-        <Heading level="2">Login</Heading>
-        <Anchor href="/register" label="or Create An Account"></Anchor>
-      </Box>
+      <LogoHeader />
 
-      <Form
-        id="login-form"
-        validate="blur"
-        onSubmit={({ value }) => {
-          login(value, showError, setLoading, setCurrentUser, setCurrentTokens)
-        }}
-      >
-        {/* Redirected Message */}
-        <Box background={{ color: 'status-ok', opacity: 'weak' }} pad={{ horizontal: 'small' }}>
-          {redirectedMessage && <Message message={redirectedMessage} />}
+      <FormContainer>
+        <Box align="center" data-testid="login-header">
+          <Heading level="2">Login</Heading>
+          <Link to="/register">or Create An Account</Link>
         </Box>
 
-        <FormField
-          label="Email"
-          name="email"
-          required
-          validate={[
-            email => {
-              if (email && !isEmail(email)) return 'Please enter a valid email address'
-              return undefined
-            },
-          ]}
+        <Form
+          id="login-form"
+          validate="blur"
+          onSubmit={({ value }) => {
+            login(value, showError, setLoading, setCurrentUser, setCurrentTokens)
+          }}
         >
-          <TextInput name="email" type="email" />
-        </FormField>
+          {/* Redirected Message */}
+          <Box
+            background={{ color: 'accent-1', opacity: 'weak' }}
+            margin={{ vertical: 'small' }}
+            pad={{ horizontal: 'small' }}
+          >
+            {redirectedMessage && <Message message={redirectedMessage} />}
+          </Box>
 
-        <FormField
-          label="Password"
-          name="password"
-          required
-          validate={[
-            password => {
-              if (password && password.length <= 8) return 'Please enter more than 8 characters'
-              return undefined
-            },
-          ]}
-        >
-          <PasswordInput id="password" name="password" />
-        </FormField>
+          <FormField
+            label="Email"
+            name="email"
+            required
+            validate={[
+              email => {
+                if (email && !isEmail(email)) return 'Please enter a valid email address'
+                return undefined
+              },
+            ]}
+          >
+            <TextInput name="email" type="email" />
+          </FormField>
 
-        {/* Submit */}
-        <Box direction="row" margin={{ vertical: 'medium' }}>
-          <Button type="submit" label="Login" disabled={_isButtonDisabled()} />
-        </Box>
+          <FormField
+            label="Password"
+            name="password"
+            required
+            validate={[
+              password => {
+                if (password && password.length <= 8) return 'Please enter more than 8 characters'
+                return undefined
+              },
+            ]}
+          >
+            <PasswordInput id="password" name="password" />
+          </FormField>
 
-        <Link to="/password-reset-request">Forgot Password?</Link>
+          {/* Submit */}
+          <Box direction="row" fill="horizontal" margin={{ vertical: 'medium' }}>
+            <Button
+              data-testid="submit-button"
+              disabled={loading}
+              fill="horizontal"
+              label="Login"
+              primary
+              type="submit"
+            />
+          </Box>
 
-        {/* Status Messages */}
-        <Box>{error && <Message message={error} isError />}</Box>
-      </Form>
+          <Link to="/password-reset-request">Forgot Password?</Link>
+
+          {/* Status Messages */}
+          <Box>{error && <Message message={error} isError />}</Box>
+        </Form>
+      </FormContainer>
     </>
   )
 }
