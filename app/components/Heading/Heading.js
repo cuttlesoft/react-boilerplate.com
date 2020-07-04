1f564c1ac4d2270895658a9efadddfb0de587a99
Emily Morehouse <emily@cuttlesoft.com>
2020-07-03 22:27:45 -0600

    [CUTL-90] 🔄 (Heading) Add support for `caps` prop; default to `small` size

--- app/components/Heading/Heading.js
+++ app/components/Heading/Heading.js
@@ -8,12 +8,28 @@ import { Heading as GrommetHeading } from 'grommet'
 import { doc } from './Heading.doc'
 import { createWithDoc } from '../../utils/helpers'
 
+// Styles
+import { HEADER_FONT } from '../../utils/theme'
+
 /**
  *
  * Heading
  *
  */
-const Heading = ({ children, ...rest }) => <GrommetHeading {...rest}>{children}</GrommetHeading>
+const Heading = ({ caps = false, children, size = 'small', style, ...rest }) => (
+  <GrommetHeading
+    size={size}
+    style={{
+      fontFamily: `${HEADER_FONT}, 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+      maxWidth: '100vw',
+      textTransform: `${caps && 'uppercase'}`,
+      ...style,
+    }}
+    {...rest}
+  >
+    {children}
+  </GrommetHeading>
+)
 
 export default createWithDoc({
   envName: process.env.NODE_ENV,
