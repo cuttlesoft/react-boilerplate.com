008b73a2d7f3e4f2dab87763946684c12b260ec7
Emily Morehouse <emily@cuttlesoft.com>
2020-07-04 08:36:08 -0600

    [CUTL-90] 🔄 (Container, FormContainer) Pass thru arbitrary props as `rest`

--- app/components/FormContainer/FormContainer.test.js
+++ app/components/FormContainer/FormContainer.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-// import ''@testing-library/jest-dom/extend-expect' // add some helpful assertions
 
 import FormContainer from './FormContainer'
 
