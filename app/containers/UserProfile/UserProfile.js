b1fd496c085f99e0ee299b750eba8c450354deb1
Emily Morehouse <emily@cuttlesoft.com>
2020-07-03 22:51:07 -0600

    [CUTL-90] 💄 (UserProfile) Wrap in a FormContainer to ensure proper layout

--- app/containers/UserProfile/UserProfile.js
+++ app/containers/UserProfile/UserProfile.js
@@ -6,6 +6,7 @@ import { isEmail } from 'validator'
 // Components
 import { Box } from 'components/Box'
 import { Button } from 'components/Button'
+import { FormContainer } from 'components/FormContainer'
 import { Form, FormField } from 'components/Form'
 import { Image } from 'components/Image'
 import { Message } from 'components/Message'
@@ -88,142 +89,144 @@ const UserProfile = () => {
   const _isSaveDisabled = () => isEqual(user, updatedUser) || isLoading
 
   return (
-    <Form>
-      <Box align="center" margin={{ bottom: 'medium' }}>
-        {user.avatar && !editProfileImage ? (
-          <>
-            <Box height="small" width="small" round="xlarge" overflow="hidden">
-              <Image fit="contain" src={updatedUser.avatar} />
-            </Box>
-            <Button onClick={() => setEditProfileImage(true)}>Edit Profile Image</Button>
-          </>
-        ) : (
-          <ImageEditor
-            showError={showError}
-            setIsLoading={setIsLoading}
-            setEditProfileImage={setEditProfileImage}
-          />
-        )}
-      </Box>
+    <FormContainer>
+      <Form>
+        <Box align="center" margin={{ bottom: 'medium' }}>
+          {user.avatar && !editProfileImage ? (
+            <>
+              <Box height="small" width="small" round="xlarge" overflow="hidden">
+                <Image fit="contain" src={updatedUser.avatar} />
+              </Box>
+              <Button onClick={() => setEditProfileImage(true)}>Edit Profile Image</Button>
+            </>
+          ) : (
+            <ImageEditor
+              showError={showError}
+              setIsLoading={setIsLoading}
+              setEditProfileImage={setEditProfileImage}
+            />
+          )}
+        </Box>
 
-      {/* Basic Information */}
-      <Text>Basic Information</Text>
+        {/* Basic Information */}
+        <Text>Basic Information</Text>
 
-      <FormField
-        disabled={isLoading}
-        label="First Name"
-        name="firstName"
-        onChange={handleChange}
-        value={updatedUser.firstName}
-        validate={[
-          { regexp: /^[a-z]/i },
-          name => {
-            if (name && name.length === 1) return 'Please enter more than one character'
-            return undefined
-          },
-        ]}
-        required
-      >
-        <TextInput disabled={isLoading} name="firstName" />
-      </FormField>
-
-      <FormField
-        disabled={isLoading}
-        label="Last Name"
-        name="lastName"
-        onChange={handleChange}
-        value={updatedUser.lastName}
-        validate={[
-          { regexp: /^[a-z]/i },
-          name => {
-            if (name && name.length === 1) return 'Please enter more than one character'
-            return undefined
-          },
-        ]}
-        required
-      >
-        <TextInput disabled={isLoading} name="lastName" />
-      </FormField>
-
-      {/* We don't support the user changing their email address yet, so this is
-      always disabled */}
-      <FormField
-        disabled
-        label="Email"
-        name="email"
-        onChange={handleChange}
-        value={updatedUser.email}
-        validate={[
-          () => {
-            if (updatedUser.email && !isEmail(updatedUser.email))
-              return 'Please enter a valid email address'
-            return undefined
-          },
-        ]}
-        required
-      >
-        <TextInput disabled name="email" />
-      </FormField>
-
-      <FormField
-        disabled={isLoading}
-        label="Phone"
-        name="phone"
-        onChange={handleChange}
-        validate={[
-          phone => {
-            if (phone && phone.length < 14) return 'Number must be 10 digits'
-            return undefined
-          },
-        ]}
-        value={updatedUser.phone}
-      >
-        <MaskedInput
+        <FormField
           disabled={isLoading}
-          mask={[
-            { fixed: '(' },
-            {
-              length: 3,
-              regexp: /^[0-9]{1,3}$/,
-              placeholder: 'xxx',
-            },
-            { fixed: ')' },
-            { fixed: ' ' },
-            {
-              length: 3,
-              regexp: /^[0-9]{1,3}$/,
-              placeholder: 'xxx',
-            },
-            { fixed: '-' },
-            {
-              length: 4,
-              regexp: /^[0-9]{1,4}$/,
-              placeholder: 'xxxx',
+          label="First Name"
+          name="firstName"
+          onChange={handleChange}
+          value={updatedUser.firstName}
+          validate={[
+            { regexp: /^[a-z]/i },
+            name => {
+              if (name && name.length === 1) return 'Please enter more than one character'
+              return undefined
             },
           ]}
-          name="phone"
-        />
-      </FormField>
-
-      <Button
-        disabled={_isSaveDisabled()}
-        label={isLoading ? 'Loading' : 'Save'}
-        onClick={() =>
-          updateUser(updatedUser, showError, setIsLoading, showSuccess, setCurrentUser)
-        }
-      />
-
-      {/* Status Messages */}
-      {(error || success) && (
-        <Box
-          background={{ color: error ? 'status-error' : 'status-ok', opacity: 'weak' }}
-          margin={{ vertical: 'small' }}
-          pad={{ horizontal: 'small' }}
+          required
         >
-          <Message message={error || 'Profile updated!'} />
-        </Box>
-      )}
-    </Form>
+          <TextInput disabled={isLoading} name="firstName" />
+        </FormField>
+
+        <FormField
+          disabled={isLoading}
+          label="Last Name"
+          name="lastName"
+          onChange={handleChange}
+          value={updatedUser.lastName}
+          validate={[
+            { regexp: /^[a-z]/i },
+            name => {
+              if (name && name.length === 1) return 'Please enter more than one character'
+              return undefined
+            },
+          ]}
+          required
+        >
+          <TextInput disabled={isLoading} name="lastName" />
+        </FormField>
+
+        {/* We don't support the user changing their email address yet, so this is
+      always disabled */}
+        <FormField
+          disabled
+          label="Email"
+          name="email"
+          onChange={handleChange}
+          value={updatedUser.email}
+          validate={[
+            () => {
+              if (updatedUser.email && !isEmail(updatedUser.email))
+                return 'Please enter a valid email address'
+              return undefined
+            },
+          ]}
+          required
+        >
+          <TextInput disabled name="email" />
+        </FormField>
+
+        <FormField
+          disabled={isLoading}
+          label="Phone"
+          name="phone"
+          onChange={handleChange}
+          validate={[
+            phone => {
+              if (phone && phone.length < 14) return 'Number must be 10 digits'
+              return undefined
+            },
+          ]}
+          value={updatedUser.phone}
+        >
+          <MaskedInput
+            disabled={isLoading}
+            mask={[
+              { fixed: '(' },
+              {
+                length: 3,
+                regexp: /^[0-9]{1,3}$/,
+                placeholder: 'xxx',
+              },
+              { fixed: ')' },
+              { fixed: ' ' },
+              {
+                length: 3,
+                regexp: /^[0-9]{1,3}$/,
+                placeholder: 'xxx',
+              },
+              { fixed: '-' },
+              {
+                length: 4,
+                regexp: /^[0-9]{1,4}$/,
+                placeholder: 'xxxx',
+              },
+            ]}
+            name="phone"
+          />
+        </FormField>
+
+        <Button
+          disabled={_isSaveDisabled()}
+          label={isLoading ? 'Loading' : 'Save'}
+          onClick={() =>
+            updateUser(updatedUser, showError, setIsLoading, showSuccess, setCurrentUser)
+          }
+        />
+
+        {/* Status Messages */}
+        {(error || success) && (
+          <Box
+            background={{ color: error ? 'status-error' : 'status-ok', opacity: 'weak' }}
+            margin={{ vertical: 'small' }}
+            pad={{ horizontal: 'small' }}
+          >
+            <Message message={error || 'Profile updated!'} />
+          </Box>
+        )}
+      </Form>
+    </FormContainer>
   )
 }
 
