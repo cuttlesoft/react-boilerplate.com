07b94ee178bcea6264e8e091fc3b0d42cf0959b1
Emily Morehouse <emily@cuttlesoft.com>
2020-07-05 16:05:52 -0600

    [CUTL-90] ⚙️ (CI) Explicitly configure `CYPRESS_API_BASE_URL` for Cypress testing steps

--- .circleci/configure-build-vars.sh
+++ .circleci/configure-build-vars.sh
@@ -3,17 +3,14 @@
 if [ "$CIRCLE_BRANCH" == "prod" ]; then
     echo "Setting production URLS..."
     echo "export API_BASE_URL=$PROD_API_BASE_URL" >> $BASH_ENV
-    echo "export CYPRESS_API_BASE_URL=$PROD_API_BASE_URL" >> $BASH_ENV
     echo "export SENTRY_ENVIRONMENT=$CIRCLE_BRANCH" >> $BASH_ENV
 elif [ "$CIRCLE_BRANCH" == "staging" ]; then
     echo "Setting staging URLS..."
     echo "export API_BASE_URL=$STAGING_API_BASE_URL" >> $BASH_ENV
-    echo "export CYPRESS_API_BASE_URL=$STAGING_API_BASE_URL" >> $BASH_ENV
     echo "export SENTRY_ENVIRONMENT=$CIRCLE_BRANCH" >> $BASH_ENV
 else
     echo "Setting dev URLS..."
     echo "export API_BASE_URL=$DEV_API_BASE_URL" >> $BASH_ENV
-    echo "export CYPRESS_API_BASE_URL=$DEV_API_BASE_URL" >> $BASH_ENV
     echo "export SENTRY_ENVIRONMENT=dev" >> $BASH_ENV
 fi
 
