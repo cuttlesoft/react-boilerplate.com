5af336e8d3f31ed597d8b9d81ad5b8089c8f77d6
Emily Morehouse <emily@cuttlesoft.com>
2020-07-05 17:41:03 -0600

    [CUTL-90] 🚀 (CD) Configure and document deploys to GCP and Sentry release management (#21)
    * [CUTL-90] 🚀 (CD) Configure and document deploys to GCP and Sentry release management
    
    * [CUTL-90] ⚙️ (CI) Explicitly configure `CYPRESS_API_BASE_URL` for Cypress testing steps
    
    * [CUTL-90] 🚀 (CD) Attempt to fix workspace persist config
    
    * [CUTL-90] 🔊 (CI/CD) Add Slack notifications for build steps
    
    * [CUTL-90] 🧹 (CD) Clean up CircleCI config comments; enable enforcement of deploys on master/staging/prod only
    
    * [CUTL-90] 🔄 (Firebase) Ignore cache files; remove previously tracked cache file

---
+++ .circleci/configure-build-vars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$CIRCLE_BRANCH" == "prod" ]; then
+    echo "Setting production URLS..."
+    echo "export API_BASE_URL=$PROD_API_BASE_URL" >> $BASH_ENV
+    echo "export SENTRY_ENVIRONMENT=$CIRCLE_BRANCH" >> $BASH_ENV
+elif [ "$CIRCLE_BRANCH" == "staging" ]; then
+    echo "Setting staging URLS..."
+    echo "export API_BASE_URL=$STAGING_API_BASE_URL" >> $BASH_ENV
+    echo "export SENTRY_ENVIRONMENT=$CIRCLE_BRANCH" >> $BASH_ENV
+else
+    echo "Setting dev URLS..."
+    echo "export API_BASE_URL=$DEV_API_BASE_URL" >> $BASH_ENV
+    echo "export SENTRY_ENVIRONMENT=dev" >> $BASH_ENV
+fi
+
+# Always set the current git hash and short hash used for release
+echo "export SENTRY_RELEASE_GIT_HASH=$(git rev-parse --short HEAD)" >> $BASH_ENV
+echo "export SHORT_GIT_HASH=$(echo $CIRCLE_SHA1 | cut -c -7)" >> $BASH_ENV
---
+++ .circleci/configure-cypress-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$CIRCLE_BRANCH" == "prod" ]; then
+    echo "Setting production URLS..."
+    echo "export CYPRESS_API_BASE_URL=$PROD_API_BASE_URL" >> $BASH_ENV
+elif [ "$CIRCLE_BRANCH" == "staging" ]; then
+    echo "Setting staging URLS..."
+    echo "export CYPRESS_API_BASE_URL=$STAGING_API_BASE_URL" >> $BASH_ENV
+else
+    echo "Setting dev URLS..."
+    echo "export CYPRESS_API_BASE_URL=$DEV_API_BASE_URL" >> $BASH_ENV
+fi
