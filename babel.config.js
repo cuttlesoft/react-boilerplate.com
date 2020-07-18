70021c37f1ec5b9668ce23abf599e0dd6e51bb92
Emily Morehouse <emily@cuttlesoft.com>
2020-07-18 17:15:08 -0600

    [CUTL-90] 🚀 (Circle CI Config) Do not cache sw.js and related files

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
     'styled-components',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     '@babel/plugin-syntax-dynamic-import',
   ],
   env: {
