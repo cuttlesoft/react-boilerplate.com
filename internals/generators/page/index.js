3eefce1737c5e07becd9875c0a8ae9bee4ff0bb1
Emily Morehouse <emily@cuttlesoft.com>
2020-07-19 12:37:53 -0600

    [CUTL-90] 🔄 (Generators - Page) Change `container` to `page` generator; generate Cypress test spec instead of Jest

--- internals/generators/page/index.js
+++ internals/generators/page/index.js
@@ -1,21 +1,21 @@
 /**
- * Container Generator
+ * Page Generator
  */
 
 const componentExists = require('../utils/componentExists');
 
 module.exports = {
-  description: 'Add a container component',
+  description: 'Add a page component',
   prompts: [
     {
       type: 'input',
       name: 'name',
       message: 'What should it be called?',
-      default: 'EmptyContainer',
+      default: 'EmptyPage',
       validate: value => {
         if (/.+/.test(value)) {
           return componentExists(value)
-            ? 'A component or container with this name already exists'
+            ? 'A component or page with this name already exists'
             : true;
         }
 
@@ -30,16 +30,16 @@ module.exports = {
     },
     {
       type: 'confirm',
-      name: 'wantHeaders',
+      name: 'wantHelmet',
       default: false,
-      message: 'Do you want headers?',
+      message: 'Do you want to include Helmet for managing site meta-data, such as <title>?',
     },
     {
       type: 'confirm',
       name: 'connectToStore',
-      default: false,
+      default: true,
       message:
-        'Do you want to connect this container to store?',
+        'Do you want to connect this page to store?',
     },
     {
       type: 'confirm',
@@ -50,35 +50,34 @@ module.exports = {
     {
       type: 'confirm',
       name: 'wantLoadable',
-      default: true,
+      default: false,
       message: 'Do you want to load resources asynchronously?',
     },
   ],
   actions: data => {
-    // Generate Component.js, index.js, styles.js,  index.test.js
     const actions = [
       {
         type: 'add',
-        path: '../../app/containers/{{properCase name}}/{{properCase name}}.js',
-        templateFile: './container/Container.js.hbs',
+        path: '../../app/pages/{{properCase name}}/{{properCase name}}.js',
+        templateFile: './page/Page.js.hbs',
         abortOnFail: true,
       },
       {
         type: 'add',
-        path: '../../app/containers/{{properCase name}}/index.js',
-        templateFile: './container/index.js.hbs',
+        path: '../../app/pages/{{properCase name}}/index.js',
+        templateFile: './page/index.js.hbs',
         abortOnFail: true,
       },
       {
         type: 'add',
-        path: '../../app/containers/{{properCase name}}/{{properCase name}}.styles.js',
-        templateFile: './container/Container.styles.js.hbs',
+        path: '../../app/pages/{{properCase name}}/{{properCase name}}.styles.js',
+        templateFile: './page/Page.styles.js.hbs',
         abortOnFail: true,
       },
       {
         type: 'add',
-        path: '../../app/containers/{{properCase name}}/tests/{{properCase name}}.test.js',
-        templateFile: './container/Container.test.js.hbs',
+        path: '../../cypress/integration/{{properCase name}}.spec.js',
+        templateFile: './page/Page.spec.js.hbs',
         abortOnFail: true,
       },
     ];
@@ -87,8 +86,8 @@ module.exports = {
     if (data.wantMessages) {
       actions.push({
         type: 'add',
-        path: '../../app/containers/{{properCase name}}/{{properCase name}}.messages.js',
-        templateFile: './container/Container.messages.js.hbs',
+        path: '../../app/pages/{{properCase name}}/{{properCase name}}.messages.js',
+        templateFile: './page/Page.messages.js.hbs',
         abortOnFail: true,
       });
     }
@@ -96,7 +95,7 @@ module.exports = {
     if (data.wantLoadable) {
       actions.push({
         type: 'add',
-        path: '../../app/containers/{{properCase name}}/{{properCase name}}.loadable.js',
+        path: '../../app/pages/{{properCase name}}/{{properCase name}}.loadable.js',
         templateFile: './component/Component.loadable.js.hbs',
         abortOnFail: true,
       });
@@ -104,7 +103,7 @@ module.exports = {
 
     actions.push({
       type: 'format',
-      path: '/containers/',
+      path: '/pages/',
     });
 
     return actions;
