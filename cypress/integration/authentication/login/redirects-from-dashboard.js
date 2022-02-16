d547807183c5cbad025e993f3e95b825aff44aa7
Marisa Gomez <marisa@cuttlesoft.com>
2022-02-16 09:13:50 -0500

    [CUTL-111] 🔄 (Cypress) Fix test for demo purposes
    --

--- cypress/integration/authentication/login/redirects-from-dashboard.js
+++ cypress/integration/authentication/login/redirects-from-dashboard.js
@@ -8,7 +8,7 @@ When(/^I visit the dashboard \(unauthenticated\)$/, () => {
 
 Then(/^I get redirected to the login page$/, () => {
   // Ensure that two Login items are shown, one for the header and one for the button
-  cy.findAllByText('Test').then(requiredText => {
+  cy.findAllByText('Login').then(requiredText => {
     expect(requiredText.length).to.equal(2)
   })
 
